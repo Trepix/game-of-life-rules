9df4a157d807716f5ca9fc072dbc6ea55629f403
Trepix <pau.trepat.segura@gmail.com>
2017-11-19 23:30:09 +0100

    Add isAdjacent logic in Position

--- src/main/java/io/trepix/golrules/Position.java
+++ src/main/java/io/trepix/golrules/Position.java
@@ -1,5 +1,7 @@
 package io.trepix.golrules;
 
+import static java.lang.Math.*;
+
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
@@ -7,11 +9,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Position {
 
+    private static final double MAX_DISTANCE_ALLOWED_TO_BE_CONSIDERED_ADJACENT = sqrt(1+1);
+
     private final Integer x;
     private final Integer y;
 
 
     public boolean isAdjacent(Position position) {
-        return false;
+        double absoluteDistanceX = abs(this.x - position.x);
+        double absoluteDistanceY = abs(this.y - position.y);
+        double distance = sqrt(pow(absoluteDistanceX,2) + pow(absoluteDistanceY, 2));
+        return distance != 0 && distance <= MAX_DISTANCE_ALLOWED_TO_BE_CONSIDERED_ADJACENT;
     }
 }
